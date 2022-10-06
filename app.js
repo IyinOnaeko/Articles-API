511b28e37ece80b8c41ac05079f42e6b690b7845
Onaeko John <jonaeko11@gmail.com>
2022-10-06 16:33:36 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,46 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+//connect to db with mongoose
+mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true});
+
+
+const articleSchema = {
+    title: String,
+    content: String
+};
+
+
+//create new mongoose model using article schema
+const Article = mongoose.model("Article", articleSchema);
+
+
+//TODO
+
+const Article = new {
+
+}
+
+
+
+//get route
+app.get("/", function(req, res) {
+    
+})
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
