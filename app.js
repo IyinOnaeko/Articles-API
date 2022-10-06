c46afe6a0ef0db65e0973a0993c5191df27a1198
Onaeko John <jonaeko11@gmail.com>
2022-10-06 18:34:27 +0100

    add delete

--- app.js
+++ app.js
@@ -10,12 +10,12 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({
-  extended: true
+    extended: true
 }));
 app.use(express.static("public"));
 
 //connect to db with mongoose
-mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/wikiDB", { useNewUrlParser: true });
 
 
 const articleSchema = {
@@ -29,35 +29,54 @@ const Article = mongoose.model("Article", articleSchema);
 
 
 //get route
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
     res.send("welcome to the simple article API");
 })
 
 //get all articles
-app.get("/articles", function (req, res){
-    Article.find(function(err, foundArticles){
-        if(!err){
-        res.send(foundArticles);
+app.get("/articles", function (req, res) {
+    Article.find(function (err, foundArticles) {
+        if (!err) {
+            res.send(foundArticles);
         } else {
             res.send(err);
         }
-
     })
 })
 
 //create a new article and sending data to the server without a front-end
-app.post("/articles", function(req, res){
-    const newArticle = new Article ({
-        title: req.body.title,  
+app.post("/articles", function (req, res) {
+    
+    //newArticle document sourced from API Post request using postman
+    const newArticle = new Article({
+        title: req.body.title,
         content: req.body.content
     })
-    
-            newArticle.save();
-        res.send("New article saved");
-    
+
+    //save article
+    newArticle.save(function (err) {
+        if (!err) {
+            res.send("New article saved");
+        } else {
+            res.send(err);
+        }
+    });
 })
 
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+//delete articles 
+app.delete("/articles", function(req, res){
+    Article.deleteMany(function(err){
+        if(!err){
+            res.send("delete successful")
+        } else {
+            res.send(err);
+        }
+    })
+
+})
+
+
+app.listen(3000, function () {
+    console.log("Server started on port 3000");
 });
